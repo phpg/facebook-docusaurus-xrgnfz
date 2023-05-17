725aebcb3cf16b66ac5d5759e5c2dc83c7f95478
phpg <128560904+phpg@users.noreply.github.com>
2023-05-17 10:43:52 -0700

    2 files modified, 1 added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'e',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -76,7 +76,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
